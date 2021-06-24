fd91b3b47d9ec7cdc010e747abcb36425fe88603
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-24 19:13:12 +0200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>Where in the world?</h1>
+      </div>
+      <div className="img">
+        <img src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29kZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt=""/>
+      </div>
+      <div className="title">
+        <h1>South Africa</h1>
+      </div>
+      <div className="content">
+        <h3>Population: 123</h3>
+        <h3>Region: Africa</h3>
+        <h3>Capital: Gauteng</h3>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
