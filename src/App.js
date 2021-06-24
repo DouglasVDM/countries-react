cb421d8c8fcae2fdffef4c79c9f0010c5a2177b0
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-24 19:32:07 +0200

    add countryList component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import "./App.css";
+import CountryList from "./CountryList"
 
 function App() {
   return (
     <div className="container">
       <div className="heading">
-        <h1>Where in the world?</h1>
-      </div>
-      <div className="img">
-        <img src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29kZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt=""/>
-      </div>
-      <div className="title">
-        <h1>South Africa</h1>
-      </div>
-      <div className="content">
-        <h3>Population: 123</h3>
-        <h3>Region: Africa</h3>
-        <h3>Capital: Gauteng</h3>
+        <h2>Where in the world?</h2>
+        <CountryList />
       </div>
     </div>
   )
---
+++ src/CountryList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function CountryList() {
+  return (
+    <React.Fragment>
+      <div className="img">
+        <img src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29kZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" />
+      </div>
+      <div className="title">
+        <h1>South Africa</h1>
+      </div>
+      <div className="content">
+        <h3>Population: 123</h3>
+        <h3>Region: Africa</h3>
+        <h3>Capital: Gauteng</h3>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default CountryList
